ed9cb1c3f060bb060dc4bfb1cd65dac2be4fd41f
ArturPotapchik <artur.potapchik@gmail.com>
2015-05-20 15:13:18 +0300

    ArturPC

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -8,26 +8,26 @@ public class Main {
     public static void main(String[] args) {
 
         Laptops lp1 = new Laptops("Hp", 4, 1.5f);
-        System.out.println(lp1);
+       // System.out.println(lp1);
         lp1.setPower(100);
-        System.out.println(lp1);
+    //    System.out.println(lp1);
 
         Computers comp1 = new Computers("PC1", 8);
         comp1.setDevicePowerAndReturn(900);
-        System.out.println(comp1);
+      //  System.out.println(comp1);
 
         Tv tv1 = new Tv("Lg SmartTVS5500", 180, 55);
 
         Servers srv1 = new Servers("Apalon 1", 200, 16, 8);
 
-        System.out.println(srv1);
+       // System.out.println(srv1);
 
         arrayDevice[0] = tv1;
         arrayDevice[1] = srv1;
         arrayDevice[2] = lp1;
         arrayDevice[3] = comp1;
 
-          System.out.println(calculatePowerOfAllDevice(arrayDevice));
+          System.out.println("Summary power of all Device: " + calculatePowerOfAllDevice(arrayDevice) + "W" );
     }
 
     public static int calculatePowerOfAllDevice(Device arrayDevice[]) {
